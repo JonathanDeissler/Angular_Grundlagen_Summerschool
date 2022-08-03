fa315502d00cd2625b74da8d47e9758985e967dd
JonathanDeissler <jonathandeissler@gmail.com>
2022-08-03 09:17:53 +0200

    Added basic book-card Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {timeout} from "rxjs";
+import {Book} from "./models/book";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookmonkey-client';
+  title = 'Book List';
+   book : Book = {
+     title : 'Lord of the Rings',
+     author : 'J. R. R. Tolkin',
+     abstract : 'The Fellowship of the Ring is the first of three volumes of the epic novel[2] The Lord of the Rings by the English author J. R. R. Tolkien. It is followed by The Two Towers and The Return of the King. It takes place in the fictional universe of Middle-earth, and was originally published on 29 July 1954 in the United Kingdom. The volume consists of a foreword, in which the author discusses his writing of The Lord of the Rings, a prologue titled "Concerning Hobbits, and other matters", and the main narrative in Book I and Book II'
+   }
+
 }
---
+++ src/app/book-card/book-card.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Book} from "../models/book";
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss']
+})
+export class BookCardComponent implements OnInit {
+
+  @Input() content!: Book;
+
+  customStyle = {
+    color: 'darkgreen'
+  };
+
+  ngOnInit(): void {
+  }
+
+  handleDetailClick(click: MouseEvent) {
+    click.preventDefault();
+
+    console.log('Clicked Detail-Link', click)
+  }
+}
